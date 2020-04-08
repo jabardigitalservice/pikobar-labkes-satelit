18c34db520d994d66e023dcfd29f9bf2f1d4c90d
khihadysucahyo <khi.hady@gamatechno.com>
2020-04-08 15:22:04 +0700

    Added migration for alter size of access_token and refresh_token on oauth provider tables.

---
+++ backend/database/migrations/2020_04_08_075313_alter_table_oauth_providers_change_refresh_token.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterTableOauthProvidersChangeRefreshToken extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('oauth_providers', function (Blueprint $table) {
+            $table->string('refresh_token', 2000)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('oauth_providers', function (Blueprint $table) {
+            $table->string('refresh_token', 191)->change();
+        });
+    }
+}
