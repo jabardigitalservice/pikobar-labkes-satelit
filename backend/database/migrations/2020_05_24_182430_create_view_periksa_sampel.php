78a81db907de0dff40a8aa90504d6fed8b44d33a
nurisakbar <nuris.akbar@gmail.com>
2020-05-24 18:30:06 +0700

    Create 2020_05_24_182430_create_view_periksa_sampel.php

---
+++ backend/database/migrations/2020_05_24_182430_create_view_periksa_sampel.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateViewPeriksaSampel extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \DB::statement("create view view_durasi_periksa_sampel as select pr.*,p.created_at,s.waktu_pcr_sample_analyzed,s.waktu_pcr_sample_analyzed::date-p.created_at::date as durasi_periksa_sampel
+        from pasien as p
+        left join pasien_register as pr on p.id=pr.pasien_id
+        left join sampel as s on s.register_id=pr.register_id");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('view_periksa_sampel');
+    }
+}
