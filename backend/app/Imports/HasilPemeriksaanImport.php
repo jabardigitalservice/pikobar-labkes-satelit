25bb7cdb9f16232cfcd8b27de54bd3881f054cd7
ayocodingit <ayocodingit@gmail.com>
2020-10-14 16:22:17 +0700

    bugfix input hasil

--- backend/app/Imports/HasilPemeriksaanImport.php
+++ backend/app/Imports/HasilPemeriksaanImport.php
@@ -2,16 +2,12 @@
 
 namespace App\Imports;
 
-use App\Models\Kota;
-use App\Models\Pasien;
-use App\Models\Register;
 use App\Models\Sampel;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
-use Log;
 
 class HasilPemeriksaanImport implements ToCollection, WithHeadingRow
 {
@@ -30,22 +26,26 @@ class HasilPemeriksaanImport implements ToCollection, WithHeadingRow
                 if (!$row->get('no')) {
                     continue;
                 }
-                $sampel = Sampel::where('nomor_sampel','ilike','%'.$row->get('kode_sampel').'%')
-                          ->where('lab_satelit_id',$user->lab_satelit_id)->first();
+                $sampel = Sampel::where('nomor_sampel', 'ilike', '%' . $row->get('kode_sampel') . '%')
+                    ->where('lab_satelit_id', $user->lab_satelit_id)->first();
                 if (!$sampel) {
-                    $this->addError($key, "Nomor sampel '".$row->get('kode_sampel')."' tidak ditemukan");
-                } else if ($sampel->sampel_status == 'pcr_sample_analyzed') {
-                    $this->addError($key, "Nomor sampel '".$row->get('kode_sampel')."' sudah pada status " . $sampel->status->deskripsi);
-                } else if ($sampel->sampel_status != 'sample_taken') {
-                    $this->addError($key, "Nomor sampel '".$row->get('kode_sampel')."' masih pada status " . $sampel->status->deskripsi);
+                    $this->addError($key, "Nomor sampel '" . $row->get('kode_sampel') . "' tidak ditemukan");
+                } elseif ($sampel->sampel_status == 'pcr_sample_analyzed') {
+                    $this->addError($key, "Nomor sampel '" . $row->get('kode_sampel') . "' sudah pada status " . $sampel->status->deskripsi);
+                } elseif ($sampel->sampel_status != 'sample_taken') {
+                    $this->addError($key, "Nomor sampel '" . $row->get('kode_sampel') . "' masih pada status " . $sampel->status->deskripsi);
                 }
-                if (empty($row->get('interpretasi')) ) {
+                if (empty($row->get('interpretasi'))) {
                     $this->addError($key, "Interpretasi kosong");
                 }
-                if (!in_array(ucfirst(strtolower($row->get('interpretasi'))),['Positif','Negatif','Inkonklusif','Invalid'])) {
+                if (!in_array(ucfirst(strtolower($row->get('interpretasi'))), ['Positif', 'Negatif', 'Inkonklusif', 'Invalid'])) {
                     $this->addError($key, "Interpretasi tidak valid harus Positif,Negatif,Inkonklusif,Invalid");
                 }
+
                 $data = $row->toArray();
+                if (!preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/", $data['tanggal_pemeriksaan'])) {
+                    $this->addError($key, "Format Tanggal Swab pemeriksaan harus yyyy-mm-dd");
+                }
                 $keys = array_keys($data);
                 $data_ct = [];
                 foreach ($keys as $key) {
@@ -56,9 +56,8 @@ class HasilPemeriksaanImport implements ToCollection, WithHeadingRow
                         ];
                     }
                 }
-                if (is_integer($data['tanggal_pemeriksaan'])) {
-                    $data['tanggal_pemeriksaan'] = gmdate("Y-m-d",($data['tanggal_pemeriksaan'] - 25569) * 86400);
-                }
+
+                $data['tanggal_pemeriksaan'] = date("Y-m-d", strtotime($data['tanggal_pemeriksaan']));
                 $this->data[] = [
                     'no' => $data['no'],
                     'nomor_sampel' => $data['kode_sampel'],
