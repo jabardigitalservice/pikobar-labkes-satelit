574f30ae0ae051295df92d60740956766811fb4c
yoslie <65396086+yoslie@users.noreply.github.com>
2020-10-27 18:57:03 +0700

    remove console log

--- frontend/pages/registrasi-sampel/update.vue
+++ frontend/pages/registrasi-sampel/update.vue
@@ -510,10 +510,8 @@
           );
           this.$router.push("/registrasi/sampel");
         } catch (err) {
-          console.log(err);
           if (err.response && err.response.data.code == 422) {
             this.$nextTick(() => {
-              console.log(err.response.data.error);
               this.form.errors.set(err.response.data.error);
             });
             this.$toast.error("Mohon cek kembali formulir Anda", {
