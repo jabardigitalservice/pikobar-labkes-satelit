71c0b293517231f2da213e30c56154109e4bc45d
dodydes <dodydestriady@gmail.com>
2020-12-15 23:25:44 +0700

    fix api kelola akun dinkes

---
+++ backend/app/Http/Resources/TokenInfoResource.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TokenInfoResource extends JsonResource
+{
+    public function toArray($request) {
+        return [
+            'id' => $this->id,
+            'token' => $this->token,
+            'email' => $this->email,
+            'username' => $this->user->username,
+            'name' => $this->user->name,
+            'admin_dinkes' => $this->user->koordinator,
+            'dinkes' => $this->user->lab_satelit->nama,
+            'user' => $this->user
+        ];
+    }
+}
