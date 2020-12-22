afb9e12aff20e954179116c4f58c6cd691c28ec1
ayocodingit <ayocodingit@gmail.com>
2020-12-22 20:21:11 +0700

    cleancode-3

--- backend/app/Exceptions/Handler.php
+++ backend/app/Exceptions/Handler.php
@@ -5,17 +5,17 @@ namespace App\Exceptions;
 // use Exception;
 use App\Traits\ApiResponser;
 // use Asm89\Stack\CorsService;
-use Illuminate\Database\QueryException;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Session\TokenMismatchException;
 use Illuminate\Validation\ValidationException;
-use Illuminate\Auth\Access\AuthorizationException;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
-use Symfony\Component\Routing\Exception\RouteNotFoundException;
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Exception\RouteNotFoundException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -78,84 +78,51 @@ class Handler extends ExceptionHandler
 
     public function handleException($request, Throwable $exception)
     {
-        if ($exception instanceof ValidationException)
-        {
+        if ($exception instanceof ValidationException) {
             return $this->convertValidationExceptionToResponse($exception, $request);
-        }
-
-        if ($exception instanceof ModelNotFoundException)
-        {
+        } elseif ($exception instanceof ModelNotFoundException) {
             $modelName = strtolower(class_basename($exception->getModel()));
             return $this->errorResponse(
                 "Does not exists any {$modelName} with the specified identificator",
                 404
             );
-        }
-
-        if ($exception instanceof AuthenticationException)
-        {
-            if ($this->isFrontend($request))
-            {
+        } elseif ($exception instanceof AuthenticationException) {
+            if ($this->isFrontend($request)) {
                 return redirect()->guest('login');
+            } else {
+                return $this->errorResponse('Unauthenticated', 401);
             }
-            return $this->errorResponse('Unauthenticated', 401);
-        }
-
-        if ($exception instanceof AuthorizationException)
-        {
+        } elseif ($exception instanceof AuthorizationException) {
             return $this->errorResponse($exception->getMessage(), 403);
-        }
-
-        if ($exception instanceof NotFoundHttpException)
-        {
+        } elseif ($exception instanceof NotFoundHttpException) {
             return $this->errorResponse('The specified URL cannot be found.', 404);
-        }
-
-        if ($exception instanceof RouteNotFoundException)
-        {
+        } elseif ($exception instanceof RouteNotFoundException) {
             return $this->errorResponse('The specified Route cannot be found.', 404);
-        }
-
-        if ($exception instanceof MethodNotAllowedHttpException)
-        {
+        } elseif ($exception instanceof MethodNotAllowedHttpException) {
             return $this->errorResponse('The specified method for the requests is invalid', 405);
-        }
-
-        if ($exception instanceof HttpException)
-        {
+        } elseif ($exception instanceof HttpException) {
             return $this->errorResponse(
                 $exception->getMessage(),
                 $exception->getStatusCode()
             );
-        }
-
-        if ($exception instanceof QueryException)
-        {
+        } elseif ($exception instanceof QueryException) {
             $errorCode = $exception->errorInfo[1];
-
-            if ($errorCode == 1451)
-            {
+            if ($errorCode == 1451) {
                 return $this->errorResponse(
                     'Cannot remove the resources permanently. It is related with any other resource.',
                     409
                 );
             }
-        }
-
-        if ($exception instanceof TokenMismatchException)
-        {
+        } elseif ($exception instanceof TokenMismatchException) {
             return redirect()->back()->withInput($request->input());
-        }
-
-        if (config('app.debug'))
-        {
+        } elseif (config('app.debug')) {
             return parent::render($request, $exception);
+        } else {
+            return $this->errorResponse(
+                'Unexpected Exception, Try later.',
+                500
+            );
         }
-
-        return $this->errorResponse(
-            'Unexpected Exception, Try later.',
-            500
-        );
     }
 
     /**
@@ -165,7 +132,7 @@ class Handler extends ExceptionHandler
      * @param  \Illuminate\Validation\ValidationException  $exception
      * @return \Illuminate\Http\JsonResponse
      */
-    protected function invalidJson($request, ValidationException $exception)
+    public function invalidJson($request, ValidationException $exception)
     {
         /**
          * for getting error message $exception->getMessage()
@@ -184,22 +151,20 @@ class Handler extends ExceptionHandler
      * @param  \Illuminate\Http\Request  $request
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    protected function convertValidationExceptionToResponse(ValidationException $e, $request)
+    public function convertValidationExceptionToResponse(ValidationException $e, $request)
     {
-
         $errors = $e->errors();
 
-        if ($this->isFrontend($request))
-        {
+        if ($this->isFrontend($request)) {
             return $request->ajax()
-                ? response()->json($errors, 422)
-                : redirect()->back()->withInput($request->input())->withErrors($errors);
+            ? response()->json($errors, 422)
+            : redirect()->back()->withInput($request->input())->withErrors($errors);
+        } else {
+            return $this->invalidJson($request, $e);
         }
-
-        return $this->invalidJson($request, $e);
     }
 
-    private function isFrontend($request)
+    public function isFrontend($request)
     {
         return $request->acceptsHtml() && collect($request->route()->middleware())->contains('web');
     }
