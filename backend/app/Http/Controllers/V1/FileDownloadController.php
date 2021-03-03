d27d2f933ef2cbd9e440704a5c8a4a9ae4d1b29b
ayocodingit <ayocodingit@gmail.com>
2021-03-03 10:01:12 +0700

    download templat s3

--- backend/app/Http/Controllers/V1/FileDownloadController.php
+++ backend/app/Http/Controllers/V1/FileDownloadController.php
@@ -3,18 +3,20 @@
 namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
 use Symfony\Component\HttpFoundation\Request;
 
 class FileDownloadController extends Controller
 {
     public function download(Request $request)
     {
-        $namaFile = $request->namaFile;
-        $path = public_path() . "/format/$namaFile.xlsx";
-        abort_if(!file_exists($path), 500, 'File not exists!');
-        return response()->download($path, $namaFile . '.xlsx', [
+        $namaFile = $request->input('namaFile', '');
+        $fullNameFile = $namaFile . '.xlsx';
+        $storagePath = "template/satelit/$fullNameFile";
+        abort_if(Storage::disk('s3')->missing($storagePath), 404, 'File not exists!');
+        return Storage::disk('s3')->download($storagePath, $fullNameFile, [
             'Content-Type' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-            'Content-Disposition' => 'inline; filename="' . $namaFile . '"'
+            'Content-Disposition' => 'inline; filename="' . $fullNameFile . '"'
         ]);
     }
 }
