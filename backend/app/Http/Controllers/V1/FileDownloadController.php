63ae2fbfddedccc3dcee9236ad0216acb1dca8c2
ayocodingit <ayocodingit@gmail.com>
2021-03-03 10:28:45 +0700

    download template s3

--- backend/app/Http/Controllers/V1/FileDownloadController.php
+++ backend/app/Http/Controllers/V1/FileDownloadController.php
@@ -11,12 +11,26 @@ class FileDownloadController extends Controller
     public function download(Request $request)
     {
         $namaFile = $request->input('namaFile', '');
-        $fullNameFile = $namaFile . '.xlsx';
-        $storagePath = "template/satelit/$fullNameFile";
-        abort_if(Storage::disk('s3')->missing($storagePath), 404, 'File not exists!');
+        $fullNameFile = $this->getFullNameFile($namaFile);
+        $storagePath = $this->getStoragePathFile($namaFile, $fullNameFile);
+        abort_if(Storage::disk('s3')->missing($storagePath), 422, 'File not exists!');
         return Storage::disk('s3')->download($storagePath, $fullNameFile, [
             'Content-Type' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
             'Content-Disposition' => 'inline; filename="' . $fullNameFile . '"'
         ]);
+    }
+
+    private function getFullNameFile($namaFile)
+    {
+        return $namaFile . '.xlsx';
+    }
+
+    private function getStoragePathFile($namaFile, $fullNameFile)
+    {
+        $storagePath = "template/satelit/" . $fullNameFile;
+        if (in_array($namaFile, ['fasyankes', 'labSatelit', 'wilayah'])) {
+            $storagePath = "template/data_master/" . $fullNameFile;
+        }
+        return $storagePath;
     }
 }
